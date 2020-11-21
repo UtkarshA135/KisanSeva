a1c4222c9d7b57a73e1b9dc9c6a8ad8863b1e39f
AKARSH.H.SIMHA <akarsh07simha@gmail.com>
2020-11-21 19:35:32 +0530

    NM

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -45,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
               RaisedButton(
                 onPressed: () {
                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => smartConnect()));
+                      MaterialPageRoute(builder: (context) => connect()));
                 },
                 child: Text("Smart Connect"),
               )
