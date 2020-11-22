8ac861122e0b8446f2d61e8033d1dc9439bcb64a
anu-desh <deshmukhanusha007@gmail.com>
2020-11-22 19:29:54 +0530

    rent tools template added

--- lib/screens/rent_tools/display_rent_tools.dart
+++ lib/screens/rent_tools/display_rent_tools.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kisanseva/models/rent_tools_model.dart';
+import 'package:kisanseva/screens/rent_tools/rent_tools_template.dart';
 import 'package:kisanseva/services/authservice.dart';
 
 import 'add_new.dart';
@@ -8,45 +10,41 @@ import 'desc_page.dart';
 class DisplayRentTools extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Widget> items = [
-      card("Tractor", "hdva wfdhgfwgd", "200",
-          "https://etimg.etb2bimg.com/photo/76508031.cms", context),
-      card("Harvestor", "hdva wfdhgfwgd", "300",
-          "https://tiimg.tistatic.com/fp/1/833/harvestor-864.jpg", context),
-      card(
-          "Pesticide Sprayer",
-          "hdva wfdhgfwgd",
-          "100",
-          "https://4.imimg.com/data4/ED/VM/MY-9707575/knapsack-sprayers-500x500.jpg",
-          context),
-      card(
-          "Plough",
-          "hdva wfdhgfwgd",
-          "100",
-          "https://3.imimg.com/data3/TY/KI/MY-2259091/subsoil-plough-500x500.jpg",
-          context),
-      card("Tractor", "hdva wfdhgfwgd", "200",
-          "https://etimg.etb2bimg.com/photo/76508031.cms", context),
-      card("Harvestor", "hdva wfdhgfwgd", "300",
-          "https://tiimg.tistatic.com/fp/1/833/harvestor-864.jpg", context),
-      card(
-          "Pesticide Sprayer",
-          "hdva wfdhgfwgd",
-          "100",
-          "https://4.imimg.com/data4/ED/VM/MY-9707575/knapsack-sprayers-500x500.jpg",
-          context),
-      card(
-          "Plough",
-          "hdva wfdhgfwgd",
-          "100",
-          "https://3.imimg.com/data3/TY/KI/MY-2259091/subsoil-plough-500x500.jpg",
-          context)
-    ];
+    List<Widget> items = [];
+    //   card("Harvestor", "hdva wfdhgfwgd", "300",
+    //       "https://tiimg.tistatic.com/fp/1/833/harvestor-864.jpg", context),
+    //   card(
+    //       "Pesticide Sprayer",
+    //       "hdva wfdhgfwgd",
+    //       "100",
+    //       "https://4.imimg.com/data4/ED/VM/MY-9707575/knapsack-sprayers-500x500.jpg",
+    //       context),
+    //   card(
+    //       "Plough",
+    //       "hdva wfdhgfwgd",
+    //       "100",
+    //       "https://3.imimg.com/data3/TY/KI/MY-2259091/subsoil-plough-500x500.jpg",
+    //       context),
+    //   card("Tractor", "hdva wfdhgfwgd", "200",
+    //       "https://etimg.etb2bimg.com/photo/76508031.cms", context),
+    //   card("Harvestor", "hdva wfdhgfwgd", "300",
+    //       "https://tiimg.tistatic.com/fp/1/833/harvestor-864.jpg", context),
+    //   card(
+    //       "Pesticide Sprayer",
+    //       "hdva wfdhgfwgd",
+    //       "100",
+    //       "https://4.imimg.com/data4/ED/VM/MY-9707575/knapsack-sprayers-500x500.jpg",
+    //       context),
+    //   card(
+    //       "Plough",
+    //       "hdva wfdhgfwgd",
+    //       "100",
+    //       "https://3.imimg.com/data3/TY/KI/MY-2259091/subsoil-plough-500x500.jpg",
+    //       context)
+    // ];
 
     return Scaffold(
-      drawer: Drawer(
-        
-      ),
+      drawer: Drawer(),
       appBar: AppBar(
         title: Text("SmartFarm"),
         actions: [
@@ -55,19 +53,19 @@ class DisplayRentTools extends StatelessWidget {
             color: Colors.white,
             onPressed: () {},
           ),
-                       IconButton(
-                icon: Icon(
-                  Icons.exit_to_app,
-                  color: Colors.black,
-                ),
-                onPressed: () async {
-                  AuthService().signOut();
-                  Navigator.pushReplacement(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (context) => AuthService().handleAuth()));
-                },
-              )
+          IconButton(
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.black,
+            ),
+            onPressed: () async {
+              AuthService().signOut();
+              Navigator.pushReplacement(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => AuthService().handleAuth()));
+            },
+          )
         ],
       ),
       body: Container(
@@ -167,12 +165,16 @@ class DisplayRentTools extends StatelessWidget {
               SizedBox(
                 height: 20,
               ),
-              GridView(
+              GridView.builder(
                 physics: NeverScrollableScrollPhysics(),
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2),
                 shrinkWrap: true,
-                children: items,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return items[index];
+                  // return RentToolsTemplate(rentToolsModel: model);
+                },
               )
             ],
           ),
@@ -187,61 +189,6 @@ class DisplayRentTools extends StatelessWidget {
             ),
           );
         },
-      ),
-    );
-  }
-
-  card(String name, String description, String cost, String img_url,
-      BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => DescPage(
-                name: name,
-                description: description,
-                cost: cost,
-                img_url: img_url),
-          ),
-        );
-      },
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black54,
-                  offset: Offset(1.0, 1.0),
-                  blurRadius: 10.0,
-                ),
-              ]),
-          child: Column(
-            children: [
-              Image(
-                image: NetworkImage(img_url),
-                height: 130,
-                width: 130,
-                fit: BoxFit.contain,
-              ),
-              Text(
-                name,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                "$cost/hr",
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
